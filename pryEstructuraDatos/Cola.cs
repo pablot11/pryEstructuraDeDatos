7e1a7953ae0ee8dd6c9972a4d53a80d786970772
Alumnos <Alumnos@PC176>
2023-03-31 11:38:04 -0300

    ddd

---
+++ pryEstructuraDatos/Cola.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryEstructuraDatos
+{
+    internal class Cola
+    {
+
+        public Nodo Primero;
+        public Nodo Ultimo;
+
+        public void Agregar(Nodo Nuevo)
+            //  Pregunto si no hay ningun registro
+            //Que el nuevo tome ambos valores
+        {
+            if (Primero == null)
+            {
+                Primero = Nuevo;
+                Ultimo = Nuevo;
+            }
+            else
+            {
+                Ultimo.Siguiente = Nuevo;
+                Ultimo = Nuevo;
+            }
+
+        }
+
+
+
+    }
+}
